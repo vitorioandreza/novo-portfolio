19d6c56c96fb85e3c0c44632f4a20902d59184d8
Andreza Vitório <andreza@crpmango.com.br>
2025-03-05 16:44:26 -0300

    Atualização nas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,9 @@ const routes: Routes = [
 	{ path: 'sobre-mim',   loadChildren: () => import('@app/about/about.module').then(m => m.AboutModule), 			   data: {animation: 'about-me'} },
 	{ path: 'habilidades', loadChildren: () => import('@app/abilities/abilities.module').then(m => m.AbilitiesModule), data: {animation: 'abilities'} },
 	{ path: 'projetos',    loadChildren: () => import('@app/projects/projects.module').then(m => m.ProjectsModule),    data: {animation: 'projects'} },
-	{ path: 'home',		   loadChildren: () => import('@app/home/home.module').then(m => m.HomeModule), 			   data: {animation: 'home'} },
+	{ path: 'inicio',		   loadChildren: () => import('@app/home/home.module').then(m => m.HomeModule), 			   data: {animation: 'home'} },
 	
-	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: '', redirectTo: '/inicio', pathMatch: 'full' },
 	{ path: '**', loadChildren: () => import('@app/page-not-found/page-not-found.module').then(m => m.PageNotFoundModule), data: {animation: 'not-found'} }
 ];
 export const AppRouting = RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, anchorScrolling: 'enabled' });
